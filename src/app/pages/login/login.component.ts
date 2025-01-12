edcc97737a1c53b1ff5e643e345f4f190dc7b157
Fatah ZAMICHE <zamiche.fatah@gmail.com>
2025-01-12 14:19:03 +0100

    Connect component.ts to HTML and fix error handling

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,46 +4,42 @@ import {Router} from "@angular/router";
 import {AuthenticationService} from "../../services/services/authentication.service";
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
 
-    authRequest: AuthenticationRequest = {email: '', password: ''};
-    errorMsg: Array<string> = [];
+  authRequest: AuthenticationRequest = {email: '', password: ''};
+  errorMsg: Array<string> = [];
 
-    constructor(
-        private router: Router,
-        private authService: AuthenticationService) {
-    }
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService) {
+  }
 
-    login() {
-        this.errorMsg = [];
-        this.authService.authenticate({ body: this.authRequest }).subscribe({
-            next: (res) => {
-                this.router.navigate(['books']);
-            },
-            error: (error) => {
-                if (error.error instanceof Blob) {
-                    // Convertir Blob en JSON
-                    error.error.text().then((text: string) => {
-                        const errorJson = JSON.parse(text);
-                        if (errorJson.validationErrors) {
-                            this.errorMsg = errorJson.validationErrors;
-                        } else {
-                            this.errorMsg.push("Une erreur inconnue s'est produite.");
-                        }
-                    });
-                } else {
-                    this.errorMsg.push("Une erreur inconnue s'est produite.");
-                }
-            }
-        });
-    }
+  login() {
+    this.errorMsg = [];
+    this.authService.authenticate({
+      body: this.authRequest
+    }).subscribe({
+      next: () => {
+        // save token
+        this.router.navigate(['books']);
+      },
+      error: (err) => {
+        console.log(err);
+        if (err.error.validationErrors) {
+          this.errorMsg = err.error.validationErrors;
+        } else {
+          this.errorMsg.push(err.error.error);
+        }
+      }
+    });
+  }
 
 
-    register() {
-        this.router.navigate(['register']);
-    }
+  register() {
+    this.router.navigate(['register']);
+  }
 }
