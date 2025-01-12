2a35df2d58d669adca1407f93961419088b28b56
Fatah ZAMICHE <zamiche.fatah@gmail.com>
2025-01-12 16:57:01 +0100

    Impl the registration page

---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,38 @@
+import {Component} from '@angular/core';
+import {RegistrationRequest} from "../../services/models/registration-request";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/services/authentication.service";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+
+  registerRequest: RegistrationRequest = {email: '', password: '', firstname: '', lastname: ''};
+  errorMsg: Array<string> = [];
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService) {
+  }
+
+  register() {
+    this.errorMsg = [];
+    this.authService.registrer({
+      body: this.registerRequest
+    }).subscribe({
+        next: () => {
+          this.router.navigate(['/activate-account']);
+        },
+        error: (error) => {
+          this.errorMsg = error.error.validationErrors;
+        }
+      })
+  }
+
+  login() {
+    this.router.navigate(['/login']);
+  }
+}
