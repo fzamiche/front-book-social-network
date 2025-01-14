996fb1d37233fec0b0993e3939334e469324d3fc
Fatah ZAMICHE <zamiche.fatah@gmail.com>
2025-01-14 22:55:11 +0100

    Revert "Inject the `jwt-token` using an HTTP interceptor"
    This reverts commit 569cfe569025ba045c93a30e9e988752f5f029dc.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from "@angular/common/http";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 import { LoginComponent } from './pages/login/login.component';
 import {FormsModule} from "@angular/forms";
 import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 import {CodeInputModule} from "angular-code-input";
 import { RegisterComponent } from './pages/register/register.component';
-import {HttpJwtTokenInterceptor} from "./services/interceptor/http-jwt-token.interceptor";
 
 @NgModule({
   declarations: [
@@ -26,12 +25,7 @@ import {HttpJwtTokenInterceptor} from "./services/interceptor/http-jwt-token.int
     CodeInputModule
   ],
   providers: [
-    HttpClient,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: HttpJwtTokenInterceptor,
-      multi: true
-    }
+    HttpClient
   ],
   bootstrap: [AppComponent]
 })
