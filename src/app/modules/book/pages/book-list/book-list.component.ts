da891bd4d526b168248a4328b4a942f49160bc84
Fatah ZAMICHE <99917713+fzamiche@users.noreply.github.com>
2025-01-14 22:58:29 +0100

    [feature] [book-module] Add the book-list component of book module #9

---
+++ src/app/modules/book/pages/book-list/book-list.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {BookService} from "../../../../services/services/book.service";
+import {Router} from "@angular/router";
+import {PageResponseBookResponse} from "../../../../services/models/page-response-book-response";
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss']
+})
+export class BookListComponent implements OnInit {
+
+  public page: number = 0;
+  public size: number = 5;
+  public bookResponse: PageResponseBookResponse = {};
+
+  constructor(
+    private bookService: BookService,
+    private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.findAllBooks();
+  }
+
+  private findAllBooks() {
+    this.bookService.findAllBooks({
+      page: this.page,
+      size: this.size
+    }).subscribe({
+      next: (books) => {
+        this.bookResponse = books;
+      }
+    });
+  }
+}
