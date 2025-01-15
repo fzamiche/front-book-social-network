c3e4393b25a8f3aac29cf699a770ac43d7f5854e
Fatah ZAMICHE <zamiche.fatah@gmail.com>
2025-01-15 18:48:30 +0100

    [refactor] using getter/setter in `rating` component

--- src/app/modules/book/components/rating/rating.component.ts
+++ src/app/modules/book/components/rating/rating.component.ts
@@ -1,28 +1,41 @@
 import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'app-rating',
-  templateUrl: './rating.component.html',
-  styleUrls: ['./rating.component.scss']
+    selector: 'app-rating',
+    templateUrl: './rating.component.html',
+    styleUrls: ['./rating.component.scss']
 })
 export class RatingComponent {
 
-  @Input() rating: number = 0;
-  maxRating: number = 5;
+    private _rating: number = 0;
+    private _maxRating: number = 5;
 
-  // sert à savoir combien d'étoiles pleines on doit afficher
-  get fullStars(): number {
-    return Math.floor(this.rating); // arrondir à l'entier inférieur ex : 3.5 => 3
-  }
+    get rating(): number {
+        return this._rating;
+    }
 
-  // sert à savoir si on doit afficher une demi étoile
-  get hasHalfStar(): boolean {
-    return this.rating % 1 !== 0; //
-  }
+    @Input()
+    set rating(value: number) {
+        this._rating = value;
+    }
 
-  // sert à savoir combien d'étoiles vides on doit afficher
-  get emptyStars(): number {
-    return this.maxRating - Math.ceil(this.rating); // arrondir à l'entier supérieur ex : 3.5 => 4
-  }
+    get maxRating(): number {
+        return this._maxRating;
+    }
+
+    // sert à savoir combien d'étoiles pleines on doit afficher
+    get fullStars(): number {
+        return Math.floor(this.rating); // arrondir à l'entier inférieur ex : 3.5 => 3
+    }
+
+    // sert à savoir si on doit afficher une demi étoile
+    get hasHalfStar(): boolean {
+        return this.rating % 1 !== 0; //
+    }
+
+    // sert à savoir combien d'étoiles vides on doit afficher
+    get emptyStars(): number {
+        return this.maxRating - Math.ceil(this.rating); // arrondir à l'entier supérieur ex : 3.5 => 4
+    }
 
 }
